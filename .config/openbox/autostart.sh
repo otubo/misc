b52575bfdf5d93681d4b15c5c0c77404d2442cf4
Eduardo Otubo <eduardo.otubo@gmail.com>
2013-03-18 13:53:57 -0300

    upding openbox config files

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -4,3 +4,5 @@ xfce4-panel &
 /opt/ibm/ibmsam/bin/ibmsaml -q & 
 #/bin/bash /opt/ibm/c4eb/wst/bin/wst-applet & 
 feh --bg-scale Pictures/space-wallpaper2-2560x1600.jpg
+eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
+
