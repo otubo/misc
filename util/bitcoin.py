9584a7a3c1af9dc5e77077b4276317612a61ec82
Eduardo Otubo <eduardo.otubo@gmail.com>
2014-01-22 22:25:58 -0200

    adding bitcoin updater script
    this script gets the CEX balance and calculates the value in BRL
    using the latest value from mercadobitcoin and send a pushover
    notification to your device.

---
+++ util/bitcoin.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import cexapi
+import requests
+import json
+
+username = "otubo"
+api_key = "CEX api key"
+api_secret = "CEX api secret"
+pushover_token = "Pushover token"
+pushover_user_token = "Pushover user token"
+
+cex_account = cexapi.api(username, api_key, api_secret)
+balance = cex_account.balance()["BTC"]["available"]
+
+mercadobitcoin_get = requests.get("http://www.mercadobitcoin.com.br/api/ticker")
+bitcoin_raw = mercadobitcoin_get.content
+bitcoin = json.loads(bitcoin_raw)["ticker"]["last"]
+
+valor_em_reais = "%.2f" % (float(bitcoin) * float(balance))
+
+message = "Hello master, your balance is R$" + str(valor_em_reais)
+
+pushover_url='https://api.pushover.net/1/messages.json'
+data= {
+    'token': pushover_token,
+    'user': pushover_user_token,
+    'message': message
+}
+
+requests.post(pushover_url, data=data)
+
+exit
