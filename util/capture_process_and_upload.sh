d60cb22c8cf7b9e7de0207ea8055d30c4b17778e
Eduardo Otubo <eduardo.otubo@profitbricks.com>
2016-12-12 15:29:44 +0100

    fixing if's and redirections

--- util/capture_process_and_upload.sh
+++ util/capture_process_and_upload.sh
@@ -12,16 +12,17 @@ SHOW_ID="##################";
 OAUTH_TOKEN="##################";
 EPISODE_SIZE="90s"; # 90 minutes
 LAST_TS_FILE="";
+strike=0;
 
 # while we don't have any results, keep trying, for up to 3 times
 while test ! ${LAST_TS_FILE}; do
 	# save the radio stream from internet into a Transport Stream format (.ts)
 	timeout -k 1m ${EPISODE_SIZE} ${FFMPEG} -i ${URL} -c copy -strict 2 \
-		${BASE}${TIMESTAMP}.ts &> /dev/null
+		${BASE}${TIMESTAMP}.ts >/dev/null 2>&1
 	LAST_TS_FILE="$(ls -1t ${BASE}|grep ts|head -1)";
 
 	test ${LAST_TS_FILE} || ((strike++));
-	if [[ $strike = 3 ]]; then
+	if [ $strike -eq 3 ]; then
 		#you're out
 		echo "Stream da bandnews está fora; mais sorte amanhã :/" | \
 			mail -s "BAND NEWS OFFLINE" eduardo.otubo@gmail.com >/dev/null 2>&1
@@ -35,16 +36,16 @@ LAST_TS_FILE="${BASE}${LAST_TS_FILE}"
 NEW_MP3_FILE="${BASE}${NEW_MP3_FILE}"
 
 # encode the .ts file into .mp3
-${FFMPEG} -i "${LAST_TS_FILE}" -acodec mp3 -write_xing 0 "${NEW_MP3_FILE}" &> /dev/null
+${FFMPEG} -i "${LAST_TS_FILE}" -acodec mp3 -write_xing 0 "${NEW_MP3_FILE}" >/dev/null 2>&1
 
 # set id3 tags
-${ID3} -t "$(date)" "${NEW_MP3_FILE}" &> /dev/null
-${ID3} -a ${DATE} "${NEW_MP3_FILE}" &> /dev/null
-rm "${LAST_TS_FILE}" &> /dev/null
+${ID3} -t "$(date)" "${NEW_MP3_FILE}" >/dev/null 2>&1
+${ID3} -a ${DATE} "${NEW_MP3_FILE}" >/dev/null 2>&1
+rm "${LAST_TS_FILE}" >/dev/null 2>&1
 
 # upload to my spreaker account
 curl -X POST -H "Authorization: Bearer ${OAUTH_TOKEN}" -F media_file=@${NEW_MP3_FILE} -F \
-	"title=${DATE}" https://api.spreaker.com/v2/shows/${SHOW_ID}/episodes &> /dev/null
+	"title=${DATE}" https://api.spreaker.com/v2/shows/${SHOW_ID}/episodes >/dev/null 2>&1
 
 # remove the file, we don't need it anyway
 rm ${NEW_MP3_FILE} -f;
@@ -54,10 +55,10 @@ N_EPISODES_ONLINE=$(curl -s https://api.spreaker.com/v2/shows/${SHOW_ID}/episode
 	'.response'|jq '.items'|jq '.[] | .episode_id'|wc -l);
 
 # we don't have much space, so if more than 2, remove the last one
-if [[ ${N_EPISODES_ONLINE} > 2 ]]; then
+if [ ${N_EPISODES_ONLINE} -lt 2 ]; then
 	OLDEST=$(curl -s https://api.spreaker.com/v2/shows/${SHOW_ID}/episodes|jq -S \
 		'.response'|jq '.items'|jq '.[] | .episode_id'|head -n1);
 	curl -X DELETE -H "Authorization: Bearer ${OAUTH_TOKEN}" \
-		https://api.spreaker.com/v2/episodes/${OLDEST} &> /dev/null;
+		https://api.spreaker.com/v2/episodes/${OLDEST} >/dev/null 2>&1;
 fi
 	
